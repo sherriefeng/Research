43812c451b487297d62f35b6ea11274d14888396
Sherrie Feng <sherriefeng9@gmail.com>
2023-07-19 19:03:37 -0400

    Changed dl_sims to have upperbound on # trial graphs and modified data recording

--- code/simulation/run_dl_sims.py
+++ code/simulation/run_dl_sims.py
@@ -8,9 +8,10 @@ import divisionGame
 
 th = 0 # property threshold
 
-ofile = csv.writer(open("data_dl_random.csv", "w"))
-ofile.writerows([["gameId", "size", "ave_rate", "sd_rate"]])
-
+t_file = csv.writer(open("data_trial_random.csv", "w"))
+s_file = csv.writer(open("data_summary_random.csv", "w"))
+t_file.writerows([["size", "gameId", "ave_rate", "sd_rate"]])
+s_file.writerows([["size", "ave_rate", "sd_rate"]])
 
 def calculate_node_completion_rate(G, num_trials):
     total_nodes = len(G.nodes())
@@ -38,24 +39,31 @@ def calculate_node_completion_rate(G, num_trials):
         rate = divisionGame.runWithDL3(G, D, th)
         Rate.append(1.0 * rate[-1] / total_nodes) # DoL function returns # of incomplete nodes
     
-    return 1 - np.mean(Rate)
+    return Rate
 
 def main():
     for n in range(5, 21): # Loop over graphs of size 5 to 20 nodes
         directory = "../../data/networks/random_" + str(n)
+        summary_rates = []
 
         for k, filename in enumerate(os.listdir(directory)):
-            if filename.endswith(".edgelist"):
+            if filename.endswith(".edgelist") and k < 100: # Can adjust upper limit on k (graph #) here
                 print(filename)
                 G = nx.read_edgelist(os.path.join(directory, filename), nodetype=int)
 
                 # Run the count_solutions function multiple times with the same G and D
                 num_trials = 50
-                node_completion_rate = calculate_node_completion_rate(G, num_trials)
+                trial_rates = calculate_node_completion_rate(G, num_trials)
+                node_completion_rate = 1 - np.mean(trial_rates)
                 print("Avg node completion rate:", node_completion_rate)
+
+                summary_rates.append(node_completion_rate)
             
-            # RECORD DATA TO LCOAL FILE HERE
-            ofile.writerows([[n, k, np.mean(node_completion_rate), np.std(node_completion_rate)]])
+            # RECORD TRIAL DATA TO LOCAL FILE HERE
+            t_file.writerows([[n, k, node_completion_rate, np.std(trial_rates)]])
+            
+        # RECORD SUMMARY DATA TO LOCAL FILE
+        s_file.writerows([[n, np.mean(summary_rates), np.std(summary_rates)]])
 
 if __name__ == "__main__":
     main()
