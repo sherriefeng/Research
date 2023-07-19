fed27bb4532db199a09b6d16d3bac2bc4983be41
annewithoute <annchen@u.yale-nus.edu.sg>
2023-07-19 12:13:11 -0400

    modify the completion rate func + use random network

--- code/simulation/final_execution_DOL_solutions.py
+++ code/simulation/final_execution_DOL_solutions.py
@@ -313,10 +313,271 @@
 
 
 
-
+# The code that generates completion rate (first version)
 ##################################################################################################################################
 
 
+# import copy
+# import divisionGame_ann_commented
+# import networkx as nx
+# import random
+# import numpy as np
+# import csv
+# import numOfSolutionsDepthC
+# import divisionGame
+
+# ofile = csv.writer(open("../../data/data_pref_attach_2_without_storage_item3.csv", "w"))
+# ofile.writerows([["gameId", "size", "#solutions", "ave_rate", "sd_rate"]])
+
+# n = 3 # number of nodes
+# th = 0 # threshold that represents storage capacity
+
+# def count_solutions(G, D, num_trials):
+#     solution_counts = 0
+#     for _ in range(num_trials):
+#         G_copy = copy.deepcopy(G)
+#         D_copy = copy.deepcopy(D)
+#         result = divisionGame_ann_commented.runWithDL3(G_copy, D_copy, th)
+      
+#         # Check if division of labor was achieved
+#         if result[-1] == 0:  # Division of labor was achieved
+#             print("achieve DOL")
+#             solution_counts += 1
+#     return solution_counts
+
+# def calculate_node_completion_rate(G, D, num_trials):
+#     total_nodes = len(G.nodes())
+#     total_colors = set(D.values())  # total distinct colors in the graph
+#     node_wins = 0
+
+#     for _ in range(num_trials):
+#         G_copy = copy.deepcopy(G)
+#         D_copy = copy.deepcopy(D)
+#         divisionGame_ann_commented.runWithDL3(G_copy, D_copy, th)
+
+#         for node in D_copy:
+#             neighbor_colors = set(D_copy[neighbor] for neighbor in G_copy.neighbors(node))
+
+#             # Increment node wins if the neighbors collectively possess all colors
+#             if total_colors.issubset(neighbor_colors):
+#                 node_wins += 1
+
+#     # Calculate and return the completion rate
+#     return node_wins / (total_nodes * num_trials)
+
+# for k in range(500):
+#     print(k)
+#     filename = '../../data/networks/pa_2/pa_2_2_' + str(k) + '.edgelist'
+#     dataFile = csv.reader(open(filename, 'r'))
+
+#     G = nx.Graph()
+#     for data in dataFile:
+#         data = list(map(lambda x:x.strip(), data))
+#         data2 = data[0].split(" ")
+#         u1 = int(data2[0])
+#         u2 = int(data2[1])
+#         G.add_edge(u1, u2)
+
+#     D = {}
+#     for gn in list(G.nodes()):
+#         D[gn] = 0
+
+#     # Initialize node color for 3 items
+#     cycls_3 = [c for c in nx.cycle_basis(G) if len(c)==3]
+#     e = random.choice(cycls_3)
+#     D[e[0]] = 1
+#     D[e[1]] = 2
+#     D[e[2]] = 3
+
+#     # Run the count_solutions function multiple times with the same G and D
+#     num_trials = 100
+#     # solution_counts = count_solutions(G, D, num_trials)
+#     # print("solution counts:", solution_counts)
+
+#     node_completion_rate = calculate_node_completion_rate(G, D, num_trials)
+#     print("Node completion rate:", node_completion_rate)
+
+# NetworkX's Barabási–Albert graph generator
+# The issue is that both the Barabasi-Albert and Watts-Strogatz models do not 
+# guarantee cycles of length 3, which are required by our current color 
+# initialization logic.
+##################################################################################################################################
+
+# import copy
+# import divisionGame_ann_commented
+# import networkx as nx
+# import random
+# import numpy as np
+# import csv
+# import numOfSolutionsDepthC
+# import divisionGame
+
+# ofile = csv.writer(open("../../data/data_pref_attach_2_without_storage_item3.csv", "w"))
+# ofile.writerows([["gameId", "size", "#solutions", "ave_rate", "sd_rate"]])
+
+# n = 4  # number of nodes
+# m = 3  # number of edges to attach from a new node to existing nodes
+# th = 0  # threshold that represents storage capacity
+
+# for k in range(500):
+#     print(k)
+    
+#     # Generate a Barabási–Albert graph instead of reading from an .edgelist file
+#     G = nx.barabasi_albert_graph(n, m)
+
+#     D = {}
+#     for gn in list(G.nodes()):
+#         D[gn] = 0
+
+#     # Initialize node color for 3 items
+#     cycls_3 = [c for c in nx.cycle_basis(G) if len(c)==3]
+#     if not cycls_3:
+#         continue
+#     e = random.choice(cycls_3)
+#     D[e[0]] = 1
+#     D[e[1]] = 2
+#     D[e[2]] = 3
+
+#     # Run the count_solutions function multiple times with the same G and D
+#     num_trials = 100
+
+#     node_completion_rate = calculate_node_completion_rate(G, D, num_trials)
+#     print("Node completion rate:", node_completion_rate)
+
+##################################################################################################################################
+# use a different model that should create more cycles. 
+# The nx.newman_watts_strogatz_graph function should do a better job at this, 
+# as it doesn't rewire edges but instead adds new edges with a probability p. 
+# This should increase the chance of cycles of length 3.
+
+# import copy
+# import divisionGame_ann_commented
+# import networkx as nx
+# import random
+# import numpy as np
+# import csv
+# import numOfSolutionsDepthC
+# import divisionGame
+
+# ofile = csv.writer(open("../../data/data_pref_attach_2_without_storage_item3.csv", "w"))
+# ofile.writerows([["gameId", "size", "#solutions", "ave_rate", "sd_rate"]])
+
+# n = 20  # number of nodes
+# k = 4  # each node is connected to k nearest neighbors in ring topology
+# p = 0.5  # the probability of adding a new edge for each edge
+# th = 0  # threshold that represents storage capacity
+
+
+# def calculate_node_completion_rate(G, D, num_trials):
+#     total_nodes = len(G.nodes())
+#     total_colors = set(D.values())  # total distinct colors in the graph
+#     node_wins = 0
+
+#     for _ in range(num_trials):
+#         G_copy = copy.deepcopy(G)
+#         D_copy = copy.deepcopy(D)
+#         divisionGame_ann_commented.runWithDL3(G_copy, D_copy, th)
+
+#         for node in D_copy:
+#             neighbor_colors = set(D_copy[neighbor] for neighbor in G_copy.neighbors(node))
+
+#             # Increment node wins if the neighbors collectively possess all colors
+#             if total_colors.issubset(neighbor_colors):
+#                 node_wins += 1
+
+#     # Calculate and return the completion rate
+#     return node_wins / (total_nodes * num_trials)
+# for i in range(500):
+#     print(i)
+    
+#     # Generate a Newman-Watts-Strogatz graph instead of reading from an .edgelist file
+#     G = nx.newman_watts_strogatz_graph(n, k, p)
+
+#     D = {}
+#     for gn in list(G.nodes()):
+#         D[gn] = 0
+
+#     # Initialize node color for 3 items
+#     cycls_3 = [c for c in nx.cycle_basis(G) if len(c)==3]
+#     if not cycls_3:
+#         continue
+#     e = random.choice(cycls_3)
+#     D[e[0]] = 1
+#     D[e[1]] = 2
+#     D[e[2]] = 3
+
+#     # Run the count_solutions function multiple times with the same G and D
+#     num_trials = 100
+
+#     node_completion_rate = calculate_node_completion_rate(G, D, num_trials)
+#     print("Node completion rate:", node_completion_rate)
+
+##################################################################################################################################
+# Sherrie's graph generator function
+
+# import copy
+# import divisionGame_ann_commented
+# import networkx as nx
+# import random
+# import numpy as np
+# import csv
+# import numOfSolutionsDepthC
+# import divisionGame
+# import os
+
+# th = 0
+
+# def calculate_node_completion_rate(G, D, num_trials):
+#     total_nodes = len(G.nodes())
+#     total_colors = set(D.values())  # total distinct colors in the graph
+#     node_wins = 0
+
+#     for _ in range(num_trials):
+#         G_copy = copy.deepcopy(G)
+#         D_copy = copy.deepcopy(D)
+#         divisionGame_ann_commented.runWithDL3(G_copy, D_copy, th)
+
+#         for node in D_copy:
+#             neighbor_colors = set(D_copy[neighbor] for neighbor in G_copy.neighbors(node))
+
+#             # Increment node wins if the neighbors collectively possess all colors
+#             if total_colors.issubset(neighbor_colors):
+#                 node_wins += 1
+
+#     # Calculate and return the completion rate
+#     return node_wins / (total_nodes * num_trials)
+
+# directory = "../../data/networks/random_10/"
+# for filename in os.listdir(directory):
+#     if filename.endswith(".edgelist"):
+#         print(filename)
+        
+#         G = nx.read_edgelist(os.path.join(directory, filename))
+
+#         D = {}
+#         for gn in list(G.nodes()):
+#             D[gn] = 0
+
+#         # Initialize node color for 3 items
+#         cycls_3 = [c for c in nx.cycle_basis(G) if len(c)==3]
+#         if not cycls_3:
+#             continue
+#         e = random.choice(cycls_3)
+#         D[e[0]] = 1
+#         D[e[1]] = 2
+#         D[e[2]] = 3
+
+#         # Run the count_solutions function multiple times with the same G and D
+#         num_trials = 100
+
+#         node_completion_rate = calculate_node_completion_rate(G, D, num_trials)
+#         print("Node completion rate:", node_completion_rate)
+
+
+
+##################################################################################################################################
+
+import os
 import copy
 import divisionGame_ann_commented
 import networkx as nx
@@ -326,51 +587,102 @@ import csv
 import numOfSolutionsDepthC
 import divisionGame
 
-ofile = csv.writer(open("../../data/data_pref_attach_2_without_storage_item3.csv", "w"))
-ofile.writerows([["gameId", "size", "#solutions", "ave_rate", "sd_rate"]])
+directory = "../../data/networks/random_10/"
 
-n = 20 # number of nodes
-th = 0 # threshold that represents storage capacity
+th = 0
 
-def count_solutions(G, D, num_trials):
-    solution_counts = 0
-    for _ in range(num_trials):
-        G_copy = copy.deepcopy(G)
-        D_copy = copy.deepcopy(D)
-        result = divisionGame_ann_commented.runWithDL3(G_copy, D_copy, th)
-      
-        # Check if division of labor was achieved
-        if result[-1] == 0:  # Division of labor was achieved
-            print("achieve DOL")
-            solution_counts += 1
-    return solution_counts
+# def calculate_node_completion_rate(G, D, num_trials):
+#     total_nodes = len(G.nodes())
+#     total_colors = set(D.values())  # total distinct colors in the graph
+#     node_wins = 0
 
-for k in range(500):
-    print(k)
-    filename = '../../data/networks/pa_2/pa_2_2_' + str(k) + '.edgelist'
-    dataFile = csv.reader(open(filename, 'r'))
+#     for _ in range(num_trials):
+#         divisionGame_ann_commented.runWithDL3(G, D, th)
 
-    G = nx.Graph()
-    for data in dataFile:
-        data = list(map(lambda x:x.strip(), data))
-        data2 = data[0].split(" ")
-        u1 = int(data2[0])
-        u2 = int(data2[1])
-        G.add_edge(u1, u2)
+#         for node in D:
+#             neighbor_colors = set(D[neighbor] for neighbor in G.neighbors(node))
 
-    D = {}
-    for gn in list(G.nodes()):
-        D[gn] = 0
+#             # Increment node wins if the neighbors collectively possess all colors
+#             if total_colors.issubset(neighbor_colors):
+#                 node_wins += 1
 
-    # Initialize node color for 3 items
-    cycls_3 = [c for c in nx.cycle_basis(G) if len(c)==3]
-    e = random.choice(cycls_3)
-    D[e[0]] = 1
-    D[e[1]] = 2
-    D[e[2]] = 3
+#     # Calculate and return the completion rate
+#     return node_wins / (total_nodes * num_trials)
 
-    # Run the count_solutions function multiple times with the same G and D
-    num_trials = 100
-    solution_counts = count_solutions(G, D, num_trials)
-    print("solution counts:", solution_counts)
+
+# def calculate_node_completion_rate(G, D, num_trials):
+#     total_nodes = len(G.nodes())
+#     node_wins = 0
+
+#     for _ in range(num_trials):
+#         divisionGame_ann_commented.runWithDL3(G, D, th)
+
+#         for node in D:
+#             neighbor_colors = set(D[neighbor] for neighbor in G.neighbors(node))
+
+#             # Increment node wins if the neighbors collectively possess all colors
+#             if total_colors.issubset(neighbor_colors):
+#                 node_wins += 1
+
+#     # Calculate and return the completion rate
+#     return node_wins / (total_nodes * num_trials)
+
+
+def calculate_node_completion_rate(G, D, num_trials):
+    total_nodes = len(G.nodes())
+    Rate = []
+    for _ in range (num_trials):
+        
+        rate = divisionGame_ann_commented.runWithDL3(G, D, th)
+        Rate.append(1.0 * rate[-1] / total_nodes)
+    return 1- np.mean(Rate)
+    # total_nodes = len(G.nodes())
+    # node_fails = 0
+
+    # for _ in range(num_trials):
+    #     R = divisionGame_ann_commented.runWithDL3(G, D, th)
+    #     # total = np.sum(R)
+    #     node_fails += R[-1]
+    
+    # return (1- (node_fails / (total_nodes * num_trials)))
+
+
+for filename in os.listdir(directory):
+    if filename.endswith(".edgelist"):
+        print(filename)
+        
+        G = nx.read_edgelist(os.path.join(directory, filename), nodetype=int)
+
+        D = {}
+        for gn in list(G.nodes()):
+            D[gn] = 0
+
+        # # Initialize node color for 3 items
+        # cycls_3 = [c for c in nx.cycle_basis(G) if len(c)==3]
+        # if not cycls_3:
+        #     continue
+        # e = random.choice(cycls_3)
+        # D[e[0]] = 1
+        # D[e[1]] = 2
+        # D[e[2]] = 3
+
+        # Initialize node color for 3 items
+        cycls_3 = [c for c in nx.cycle_basis(G) if len(c)==3]
+        if not cycls_3:
+            nodes = list(G.nodes())
+            random_nodes = np.random.choice(nodes, size=3, replace=False)
+            D[random_nodes[0]] = 1
+            D[random_nodes[1]] = 2
+            D[random_nodes[2]] = 3
+        else:
+            e = random.choice(cycls_3)
+            D[e[0]] = 1
+            D[e[1]] = 2
+            D[e[2]] = 3
+
+        # Run the count_solutions function multiple times with the same G and D
+        num_trials = 100
+
+        node_completion_rate = calculate_node_completion_rate(G, D, num_trials)
+        print("Node completion rate:", node_completion_rate)
 
--- code/simulation/generate_networks.py
+++ code/simulation/generate_networks.py
@@ -1,40 +1,85 @@
+# import networkx as nx
+# import random
+# import csv
+# import numOfSolutionsDepthC
+
+# for k in range(500):
+# 	# small world with shortcuts
+# 	# n = 42
+# 	# kk = 4
+# 	# p = 0.0
+# 	# shortcuts = 2 #6
+# 	# while 1:
+#  #        G = nx.connected_watts_strogatz_graph(n, kk, p)
+#  #        C = {}
+#  #        for gn in list(G.nodes()):
+#  #            C[gn] = CMAP[gn%(kk-1)]
+
+#  #        edges = list(G.edges())
+#  #        RE = random.sample(edges, shortcuts)
+
+#  #        lst = []
+#  #        for re in RE:
+#  #            lst.append(re[0])
+#  #            lst.append(re[1])
+#  #            G.remove_edge(re[0], re[1])
+
+#  #        while lst:
+#  #            rand1 = pop_random(lst)
+#  #            rand2 = pop_random(lst)
+#  #            G.add_edge(rand1, rand2)
+
+#  #        cp = numOfSolutionsDepthC.getNumOfSolutions(G)
+#  #        if cp == 6:
+#  #            break;
+#     #nx.write_edgelist(G, '../../data/networks/sw_shortcut2/sw_shortcut2_2_' + str(k) +'.edgelist', data=False)
+
+#     #pref attachemnt
+#     n = 20
+#     G = nx.barabasi_albert_graph(n, 2) #3
+#     #nx.write_edgelist(G, '../../data/networks/pa_2/pa_2_2_' + str(k) +'.edgelist', data=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######## Sherrie's Update
+
 import networkx as nx
 import random
 import csv
 import numOfSolutionsDepthC
+from itertools import combinations, groupby
+import matplotlib.pyplot as plt
+
+def gnp_random_connected_graph(n, p):
+    edges = combinations(range(n), 2)
+    G = nx.Graph()
+    G.add_nodes_from(range(n))
+    if p <= 0:
+        return G
+    if p >= 1:
+        return nx.complete_graph(n, create_using=G)
+    for _, node_edges in groupby(edges, key=lambda x: x[0]):
+        node_edges = list(node_edges)
+        random_edge = random.choice(node_edges)
+        G.add_edge(*random_edge)
+        for e in node_edges:
+            if random.random() < p:
+                G.add_edge(*e)
+    return G
 
 for k in range(500):
-	# small world with shortcuts
-	# n = 42
-	# kk = 4
-	# p = 0.0
-	# shortcuts = 2 #6
-	# while 1:
- #        G = nx.connected_watts_strogatz_graph(n, kk, p)
- #        C = {}
- #        for gn in list(G.nodes()):
- #            C[gn] = CMAP[gn%(kk-1)]
-
- #        edges = list(G.edges())
- #        RE = random.sample(edges, shortcuts)
-
- #        lst = []
- #        for re in RE:
- #            lst.append(re[0])
- #            lst.append(re[1])
- #            G.remove_edge(re[0], re[1])
-
- #        while lst:
- #            rand1 = pop_random(lst)
- #            rand2 = pop_random(lst)
- #            G.add_edge(rand1, rand2)
-
- #        cp = numOfSolutionsDepthC.getNumOfSolutions(G)
- #        if cp == 6:
- #            break;
-    #nx.write_edgelist(G, '../../data/networks/sw_shortcut2/sw_shortcut2_2_' + str(k) +'.edgelist', data=False)
-
-    #pref attachemnt
-    n = 20
-    G = nx.barabasi_albert_graph(n, 2) #3
-    #nx.write_edgelist(G, '../../data/networks/pa_2/pa_2_2_' + str(k) +'.edgelist', data=False)
+	n = 10
+	probability = 0.01
+	G = gnp_random_connected_graph(n, probability)
+	nx.write_edgelist(G, '../../data/networks/random_10/random_10_' + str(k) +'.edgelist', data=False)
